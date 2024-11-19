820aa1bd8093ecddb1676bf65aecd7ba883a3191
0xTitan <clement.suzat@gmail.com>
2024-11-19 23:20:21 +0100

    deploy

--- functions/index.js
+++ functions/index.js
@@ -1,64 +1,23 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
-
-// const { onRequest } = require("firebase-functions/v2/https");
-// const logger = require("firebase-functions/logger");
-
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
-
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
 const functions = require("firebase-functions");
 const path = require("path");
-
+const dotenv = require("dotenv");
 // All available logging functions
-const {debug, error} = require("firebase-functions/logger");
+const { debug, error } = require("firebase-functions/logger");
 
-// app.get("/index.html", (req, res) => {
-//   const acceptHeader = req.headers["accept"] || "";
-//   const sxgPath = path.join(__dirname, "../public/zk-sxg.web.app.index.sxg");
-//   const htmlPath = path.join(__dirname, "../public/index.html");
-//   // eslint-disable-next-line max-len
-//   log(
-//       "Request received for index.html with Accept header:",
-//       acceptHeader,
-//   );
-//   if (acceptHeader.includes("application/signed-exchange;v=b3")) {
-//     res.set("Content-Type", "application/signed-exchange;v=b3");
-//     res.set("X-Content-Type-Options", "nosniff");
-//     res.sendFile(sxgPath, (err) => {
-//       if (err) {
-//         error("Error serving SXG file:", err);
-//         res.status(500).send("Error serving SXG file");
-//       }
-//     });
-//   } else {
-//     res.sendFile(htmlPath, (err) => {
-//       if (err) {
-//         error("Error serving HTML file:", err);
-//         res.status(500).send("Error serving HTML file");
-//       }
-//     });
-//   }
-// });
+// Resolve the path to the .env file in the upper directory
+const envPath = path.resolve(__dirname, "../.env");
+
+// Load the .env file
+dotenv.config({ path: envPath });
 
 exports.serveSXG = functions.https.onRequest((req, response) => {
-  debug("Hello logs");
-  // debug("Hello logs", req);
-  // error("Hello logs");
-  const page = req.path; // This will give you the path, e.g., '/hello.html'
+  const page = req.path;
   const headers = req.headers["accept"] || "";
-  const sxgPath = path.resolve(__dirname, "./sxg/zk-sxg.web.app.hello.sxg");
-  const htmlPath = path.join(__dirname, "../public/hello.html");
+  const sxgPath = path.resolve(
+    __dirname,
+    "./sxg/" + process.env.SITE_NAME + "." + process.env.SXG_NAME
+  );
+  const htmlPath = path.join(__dirname, "../public/" + process.env.PAGE_NAME);
 
   debug("Page being called:", page);
   debug("sxgPath:", sxgPath);
@@ -76,6 +35,8 @@ exports.serveSXG = functions.https.onRequest((req, response) => {
     });
   } else {
     // eslint-disable-next-line max-len
-    response.send(`<h1>Hi !</h1></br>Page called: ${page} with headers : ${headers}`);
+    response.send(
+      `<h1>Hi !</h1></br>Page called: ${page} with headers : ${headers}`
+    );
   }
 });
